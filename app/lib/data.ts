47602e0b459b65ffc58f9398bd136ef630d733ff
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-03-17 16:23:53 -0300

    validação de formulario server component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,15 +17,7 @@ export async function fetchRevenue() {
   noStore();
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -173,7 +165,6 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
-    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
